45432c77a8d746cbd31ac541908eb63cfebc7da3
Christophe BOUCAUT <christophe.boucaut@geneanet.org>
2015-05-29 13:30:05 +0200

    Add script JS to test after.

---
+++ app/myScript.js
@@ -0,0 +1,25 @@
+window.CustomException = (function () {
+    'use strict';
+    /**
+     * Custom exception.
+     * 
+     * @param {string} message Error message.
+     */
+    function CustomException(message) {
+        this.message = message;
+    }
+    CustomException.prototype = Object.create(Error.prototype, {name: {value: 'CustomException'}});
+    CustomException.prototype.constructor = CustomException;
+})();
+
+window.addClassOnElement = (function (CustomException) {
+    'use strict';
+
+    return function (el, newClass) {
+        if (!(el instanceof HTMLElement)) {
+            throw new CustomException('the first argument must be an HTMLElement');
+        }
+
+        el.classList.add(newClass);
+    };
+})(window.CustomException);
